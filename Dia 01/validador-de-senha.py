e00bf77551f7e3b0f0298f885a784df8e66facfc
VirgilioMike <virgilio.oliveira@outlook.com>
2026-04-21 23:06:57 -0300

    adição de resultado

--- Dia 01/validador-de-senha.py
+++ Dia 01/validador-de-senha.py
@@ -1,29 +1,35 @@
 #VAriavel para receber a senha do usuário
 senha = input('Digite a senha: ')
+#Variável para contar os erros
+erros = 0
 
 #Validar se a senha tem o mínimo de 8 caracteres
 if len(senha) >= 8:
     print("Tamanho OK")
 else:
     print("Senha muito curta NOK")
+    erros = erros + 1     
 
 #Validar se tem pelo menos 1 letra maiúscula
 if any(c.isupper() for c in senha):
     print("Tem maiúscula OK")
 else:
     print("Falta maiúscula NOK")
+    erros = erros + 1
 
 #Validar se tem pelo menos 1 letra minúscula
 if any(c.islower() for c in senha):
     print("Tem minúscula OK")
 else:
     print("Falta minúscula NOK")
-
+    erros = erros + 1
+    
 #Validar se tem pelo menos 1 número
 if any(c.isdigit() for c in senha):
     print("Tem número OK")
 else:
     print("Falta número NOK")
+    erros = erros + 1
 
 #Validar se tem pelo menos 1 caractere especial
 especiais = "!@#$%^&*"
@@ -31,3 +37,9 @@ if any(c in especiais for c in senha):
     print("Tem caractere especial OK")
 else:
     print("Falta caractere especial NOK")
+    erros = erros + 1
+
+#Resultado final com base na quantidade de erros
+if erros == 0:
+    print("Senha segura")
+else:    print("Senha insegura, corrija os erros acima")
